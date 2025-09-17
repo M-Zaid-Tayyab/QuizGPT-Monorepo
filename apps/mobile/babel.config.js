5535ff69e7a09387b39fcdcf9374fc7fd43e79d6
Muhammad Zaid <zaidtayyab1@gmail.com>
2025-09-17 09:23:54 +0500

    Revert "Fixed android build issue for worklets with the vexo analytics"
    This reverts commit 8f12d9128445a27cf12ee63ce006273eeebce4b7.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,11 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      // Must come before Reanimated
-      "react-native-worklets/plugin",
-      // Keep Reanimated plugin last
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
